13f16301962aeb59be43fe117f281e3973111b5c
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2023-12-13 12:26:16 +0900

    スケジュール一覧レイアウト修正

--- resources/js/Pages/Schedules/List.vue
+++ resources/js/Pages/Schedules/List.vue
@@ -18,12 +18,16 @@ defineProps({
                 <h2 class="text-gray-900 text-lg mb-1 font-medium title-font text-center">スケジュール一覧</h2>
                 <div class="mt-6">
                     <ul class="mx-auto">
-                        <li v-for="schedule in schedules" :key="schedule.id" class=" bg-indigo-200 py-2 px-4 mb-4 rounded-md flex items-center justify-between md:w-128 w-96 mx-auto"><Link :href="route('schedule.detail',{id: schedule.id})">{{ schedule.date }}  :  {{ schedule.start_time }}　：　{{ schedule.title }}</Link></li>
+                        <Link v-for="schedule in schedules" :key="schedule.id" :href="route('schedule.detail', { id: schedule.id })">
+                            <li class="bg-teal-200 py-2 px-4 mb-4 rounded-md flex items-center justify-between md:w-128 w-96 mx-auto hover:bg-teal-100 text-lg transition-all duration-200">
+                                {{ schedule.date }} : {{ schedule.start_time }} ： {{ schedule.title }}
+                            </li>
+                        </Link>                    
                     </ul>
                 </div>
             </div>
             <div class="text-center mt-12">
-                <Link :href="route('schedule.create')" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">スケジュールを作成</Link>
+                <Link :href="route('schedule.create')" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-400 rounded text-lg transition-all duration-200">スケジュールを作成</Link>
             </div>
         </main>
         </AuthenticatedLayout>
