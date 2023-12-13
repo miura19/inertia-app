239170e2d72550e16191944ae0f45f062ce6d5d5
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2023-12-13 15:02:57 +0900

    フライトページレイアウト変更

--- resources/js/Pages/Flights/Index.vue
+++ resources/js/Pages/Flights/Index.vue
@@ -12,59 +12,61 @@ defineProps({
 </script>
 
 <template>
-    <div>
-        <Head title="Flight" />
-        <AuthenticatedLayout>
-            <section class="text-gray-600 body-font">
-                <div class="px-5 py-24 mx-auto">
-                    <div class="flex flex-col text-center w-full mb-8">
-                        <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Have a nice flight with us!</h1>
-                    </div>
-                    <div class="w-full mx-auto">
-                        <div>
-                            <p>{{ users.name }}</p>
-                        </div>
-                        <div>
-                            <p>FLIGHT</p>
-                        </div>
-                        <table class="table-auto w-full text-left whitespace-no-wrap">
-                            <thead>
-                                <tr>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">航空会社</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">出発</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">出発都市</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">到着</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">到着都市</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">クラス</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">フライト時間</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">食事</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">飛行距離</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">機材</th>
-                                    <th class="px-3 py-2 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">航空会社予約番号</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="px-3 py-2">{{ flights.airline_company }}</td>
-                                    <td class="px-3 py-2">{{ flights.departure_time }}</td>
-                                    <td class="px-3 py-2">{{ flights.departure_city }}</td>
-                                    <td class="px-3 py-2">{{ flights.arrival_time }}</td>
-                                    <td class="px-3 py-2">{{ flights.arrival_city }}</td>
-                                    <td class="px-3 py-2">{{ flights.class }}</td>
-                                    <td class="px-3 py-2">{{ flights.flight_time }}</td>
-                                    <td class="px-3 py-2">{{ flights.eat }}</td>
-                                    <td class="px-3 py-2">{{ flights.flight_distance }}</td>
-                                    <td class="px-3 py-2">{{ flights.equipment }}</td>
-                                    <td class="px-3 py-2">{{ flights.reservation_number }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div class="mt-12 text-center">
-                            <Link :href="route('schedule.list')" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">スケジュール</Link>
-                        </div>
-                    </div>
+    <Head title="Shedule Detail" />
+    <AuthenticatedLayout>
+        <main class="mx-auto p-6 w-full">
+            <div>
+                <div class="px-4 sm:px-0 text-center">
+                    <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Have a nice flight with us!</h1>
                 </div>
-            </section>
-        </AuthenticatedLayout>
-    </div>
+                <div class="mt-6 border-t border-gray-100 w-full mx-auto md:w-1/2">
+                    <dl class="divide-y divide-gray-100">
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">飛行機に乗る人のお名前</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ users.name }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">航空会社</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.airline_company }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">出発</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.departure_time }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">出発都市</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.departure_city }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">クラス</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.class }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">フライト時間</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.flight_time }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">食事</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.eat }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">飛行距離</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.flight_distance }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">機材</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.equipment }}</dd>
+                        </div>
+                        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt class="text-sm font-medium leading-6 text-gray-900">航空会社予約番号</dt>
+                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ flights.reservation_number }}</dd>
+                        </div>
+                    </dl>
+                </div>
+                <div class="mt-2 w-full mx-auto">
+                    <Link :href="route('schedule.list')" class="w-24 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-400 rounded text-lg transition-all duration-200">list</Link>
+                </div>
+            </div>
+        </main>
+    </AuthenticatedLayout>
 </template>
